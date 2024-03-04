abe03253b485a385203da2e711d2f06ece3559c8
Marcelo Fiamenghi <ma.baliu@gmail.com>
2024-03-04 15:07:44 -0300

    + Docstring

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,20 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
+
 namespace Calculator {
+    /// <summary>
+    /// Programa de calculadora.
+    /// </summary>
     class Program {
         static void Main(String[] args) {
             MenuTitulo();
             Menu();
-          
         }
 
+        /// <summary>
+        /// Exibe o título do aplicativo para servir de entrada ao iniciar.
+        /// </summary>
         static void MenuTitulo(){
             Console.Clear();
             Console.WriteLine("====================================");
             Console.WriteLine("             CALCULADORA          \n");
         }
 
+        /// <summary>
+        /// Menu de escolha e direcionamento das opções da calculadora.
+        /// </summary>
         static void Menu(){
             Console.WriteLine("O que você deseja fazer?");
             Console.WriteLine("1 - Adição");
@@ -38,6 +47,10 @@ namespace Calculator {
 
         }
 
+        /// <summary>
+        /// Recebe e transforma as duas entradas de usuários para float.
+        /// </summary>
+        /// <returns>Uma Tupla com os dois valores em floats.</returns>
         static (float, float) EntradasDeUsuario(){
             // Primeiro valor
             Console.WriteLine("Primeiro valor: ");
@@ -50,6 +63,9 @@ namespace Calculator {
             return (v1, v2);
         }
 
+        /// <summary>
+        /// Calcula a soma entre dois números.
+        /// </summary>
         static void Soma(){
             Console.Clear();
 
@@ -63,6 +79,9 @@ namespace Calculator {
             Console.Clear(); Menu();
         }
 
+        /// <summary>
+        /// Calcula a substração do primeiro número pelo segundo.
+        /// </summary>
         static void Subtracao (){
             Console.Clear();
 
@@ -76,6 +95,9 @@ namespace Calculator {
             Console.Clear(); Menu();
         }
 
+        /// <summary>
+        /// Calcula a divisão do primeiro número pelo segundo.
+        /// </summary>
         static void Divisao(){
             Console.Clear();
 
@@ -97,6 +119,9 @@ namespace Calculator {
             Console.Clear(); Menu();
         }
 
+        /// <summary>
+        /// Calcula o múltiplicação entre dois números.
+        /// </summary>
         static void Multiplicacao(){
             Console.Clear();
 
