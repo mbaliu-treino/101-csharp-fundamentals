21a0a61f02adde03ed44b6e6af6a29ee266ab6a0
Marcelo Fiamenghi <ma.baliu@gmail.com>
2024-03-04 02:29:39 -0300

    dev Calculator +Soma

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Calculator {
+    class Program {
+        static void Main(String[] args) {
+            Menu();
+
+            // Primeiro valor
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse( Console.ReadLine() );
+            // Console.WriteLine(v1.GetType());
+
+            // Segundo valor
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse( Console.ReadLine() );
+            // Console.WriteLine(v2.GetType());
+
+            Soma(v1, v2);
+
+            
+        }
+
+        static void Soma(float v1, float v2){
+            // Resultado
+            float Resultado = v1 + v2;
+            Console.WriteLine("O resultado da soma é " + Resultado);
+        }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("==================");
+            Console.WriteLine("INICIANDO CALCULADORA\n");
+            Console.WriteLine("");
+
+        }
+    }
+}
