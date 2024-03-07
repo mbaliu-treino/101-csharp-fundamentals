ae9b354013cd2aaeaaea8710752a5ea5a63f9c3e
Marcelo Fiamenghi <ma.baliu@gmail.com>
2024-03-07 14:13:40 -0300

    First structure

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -5,5 +5,27 @@ namespace TextEditor {
         static void Main(String[] args){
             Console.WriteLine("Hello World");
         }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("O que você desjea fazer?");
+            Console.WriteLine("1 - Abrir arquvio");
+            Console.WriteLine("2 - Criar novo arquvio");
+            Console.WriteLine("0 - Sair");
+
+            short option = short.Parse( Console.ReadLine() ?? "0" );
+
+            switch (option){
+                case 0: System.Environment.Exit(0); break;
+                case 1: OpenFile(); break;
+                case 2: EditFile(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void OpenFile(){}
+
+        static void EditFile(){}
     }
+
 }
