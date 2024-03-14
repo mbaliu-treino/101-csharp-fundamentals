bfa0b6c2659a85a3c49aea804a380473801961b0
Marcelo Fiamenghi <ma.baliu@gmail.com>
2024-03-14 15:20:24 -0300

    + Menu.Show

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu{
+
+        public static void Show()
+        {
+            Console.Clear();
+            
+            // Configuração do console
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+        }
+
+        static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for(int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");    
+            }
+            
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+        }
+    }
+}
