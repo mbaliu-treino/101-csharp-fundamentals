b0320ed29446302be542db9ac1649ee70ade4d6b
Marcelo Fiamenghi <ma.baliu@gmail.com>
2024-03-17 17:32:48 -0300

    + HandleMenuOption

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -12,7 +12,11 @@ namespace EditorHtml
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
             
-            DrawScreen(60, 10);
+            DrawScreen(30, 10);
+            WriteOptions();
+
+            var Option = short.Parse(Console.ReadLine());
+            HandleMenuOption(Option);
         }
 
         public static void DrawScreen(int largura, int altura)
@@ -39,6 +43,37 @@ namespace EditorHtml
             }
             Console.Write(StartChar);
             Console.Write("\n");
+        }
+
+        public static void WriteOptions(){
+            Console.SetCursorPosition(3, 2);  // coluna e linha
+            Console.WriteLine("        EDITOR HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("=========================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Escolha uma opção:");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir arquivo");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option){
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
         }
     }
 }
