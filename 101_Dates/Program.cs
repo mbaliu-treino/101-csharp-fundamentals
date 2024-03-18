6552235e34cd41f05d1581283718e0a1117c7106
Marcelo Fiamenghi <ma.baliu@gmail.com>
2024-03-18 16:47:51 -0300

    + Dates Studies

--- 101_Dates/Program.cs
+++ 101_Dates/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp{
+    class Program{
+        static void Main(string[] args){
+            var Date = Datetime.Now;
+
+        }
+    }
+}
